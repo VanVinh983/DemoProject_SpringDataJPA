446fbaab100d28bc0ae082f88a3d4e8a25cf9cda
VanVinh983 <doanvinh983@gmail.com>
2021-09-14 16:42:14 +0700

    commit native branch

--- src/test/java/com/example/SpringDataJPA/SpringDataJpaApplicationTests.java
+++ src/test/java/com/example/SpringDataJPA/SpringDataJpaApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.SpringDataJPA;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.SpringDataJPA.repo.StudentRepository;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
-
+	@Autowired
+	StudentRepository repository;
 	@Test
-	void contextLoads() {
+	void ReadStudentById() {
+		Long id = repository.findAll().get(0).getId();
+		System.out.println(repository.getStudentById(id).toString());
 	}
 
 }
